6f7f65b53cc977d7608b1245ef252e4c5d5ad220
lukasz <slaskww@gmail.com>
2019-07-17 11:37:05 +0200

    Add quit button

--- src/main/java/pl/scissors/client/ScissorsClient.java
+++ src/main/java/pl/scissors/client/ScissorsClient.java
@@ -1,5 +1,6 @@
 package pl.scissors.client;
 
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -8,64 +9,46 @@ import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
-import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
-public class ScissorsClient extends JFrame implements Runnable, ActionListener {
+public class ScissorsClient extends JFrame implements ActionListener {
 
     private Socket client;
     private BufferedReader reader;
     private PrintWriter writer;
     private int serverPort;
-    private String playersChoice = "SCISSORS";
+    private String serverIpAddress;
     JTextArea ta;
 
-    public ScissorsClient(int serverPort) {
+    public ScissorsClient(int serverPort, String serverIp) {
 
-        try (Socket newSocket = new Socket()) {
-            this.client = newSocket;
-            this.serverPort = serverPort;
-            this.ta = new JTextArea(20, 50);
 
-            this.client.connect(new InetSocketAddress("localhost", serverPort));
-            this.reader = new BufferedReader(new InputStreamReader(client.getInputStream(), StandardCharsets.UTF_8));
-            this.writer = new PrintWriter(new OutputStreamWriter(client.getOutputStream(), StandardCharsets.UTF_8));
-            createWindow();
-            run();
+        this.client = new Socket();
+        this.serverPort = serverPort;
+        this.serverIpAddress = serverIp;
+        this.ta = new JTextArea(20, 50);
+        createWindow();
+        connect();
+        initiateGame();
 
-        } catch (IOException e) {
-            System.out.println("Blad I/O");
-            e.printStackTrace();
-        }
     }
 
-    @Override
-    public void run() {
+    private void connect() {
 
         try {
+            ta.append("~Attempting a connection to " + serverIpAddress + ":" + serverPort + "\n");
+            TimeUnit.SECONDS.sleep(2);
+            this.client.connect(new InetSocketAddress(serverIpAddress, serverPort));
+            this.reader = new BufferedReader(new InputStreamReader(client.getInputStream(), StandardCharsets.UTF_8));
+            this.writer = new PrintWriter(new OutputStreamWriter(client.getOutputStream(), StandardCharsets.UTF_8));
+            ta.append("~Successfully connected to server.\n");
 
-            // String lineToSend = "PAPER";
-            String lineToGet;
-
-            lineToGet = reader.readLine();
-            System.out.println(lineToGet);
-            ta.append(lineToGet + "\n");
-
-
-            while (true) {
-                writer.println(playersChoice);
-                writer.flush();
-                lineToGet = reader.readLine();
-                ta.append(lineToGet + "\n");
-            }
-
-
-        } catch (IOException e) {
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
-
-
     }
-    private void createWindow(){
+
+    private void createWindow() {
         setTitle("Scissors - Player's console");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(800, 800);
@@ -74,6 +57,7 @@ public class ScissorsClient extends JFrame implements Runnable, ActionListener {
         add(new JScrollPane(ta));
 
         JPanel panel = new JPanel();
+        // panel.setLayout(new BorderLayout());
         panel.setBackground(new Color(71, 89, 135));
         panel.setBounds(10, 10, 10, 10);
         JButton b = new JButton("Paper");
@@ -85,28 +69,78 @@ public class ScissorsClient extends JFrame implements Runnable, ActionListener {
         b = new JButton("Stone");
         b.addActionListener(this);
         panel.add(b);
+        b = new JButton("Quit Game");
+        b.setActionCommand("Quit");
+        b.addActionListener(this);
+        panel.add(b);
         add(panel, "South");
         pack();
         setVisible(true);
 
     }
 
-    public static void main(String[] args) {
-
-        ScissorsClient client = new ScissorsClient(8012);
-
-        Thread runClient = new Thread(client);
-        runClient.start();
-    }
 
     @Override
-    public void actionPerformed(ActionEvent actionEvent) {
+    public synchronized void actionPerformed(ActionEvent actionEvent) {
 
         String cmd = actionEvent.getActionCommand();
         System.out.println(cmd);
+        String playersChoice = cmd.toUpperCase();
+        String lineToGet;
 
-        playersChoice = cmd.toUpperCase();
+        try {
+
+            writer.println(playersChoice);
+            writer.flush();
+            ta.append("Player: I get " + playersChoice + "\n");
+            lineToGet = reader.readLine();
+            ta.append(lineToGet + "\n");
+
+            if (cmd.equals("Quit")) {
+                close();
+                System.exit(0);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
     }
+
+    private void close() {
+        try {
+            client.shutdownInput();
+            client.shutdownOutput();
+            client.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void initiateGame()  {
+
+        System.out.println("in initiateGame()");
+
+
+        String lineToGet;
+        try {
+            lineToGet = reader.readLine();
+            ta.append(lineToGet + "\n");
+
+            while(reader.ready()){
+                lineToGet = reader.readLine();
+                ta.append(lineToGet + "\n");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void main(String[] args) {
+        ScissorsClient client = new ScissorsClient(8012, "127.0.0.1");
+    }
+
 }
--- src/main/java/pl/scissors/server/GameHandler.java
+++ src/main/java/pl/scissors/server/GameHandler.java
@@ -7,7 +7,6 @@ import java.security.SecureRandom;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 public class GameHandler implements Runnable {
@@ -27,44 +26,64 @@ public class GameHandler implements Runnable {
         firstDefeatsSecond = new HashMap<>();
         random = new SecureRandom();
         fillWithOptions();
-        run();
-
+        //run();
     }
 
     @Override
     public void run() {
 
-        try(Socket client = socket){
+        try {
 
-            reader = new BufferedReader(new InputStreamReader(client.getInputStream(), StandardCharsets.UTF_8));
-            writer = new PrintWriter(new OutputStreamWriter(client.getOutputStream(), StandardCharsets.UTF_8));
-
-            writer.println("Welcome to the game!\nChoose one of the following options and try to win.\nBut...choose wisely, I know what you are thinking about.\n");
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
+            writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), StandardCharsets.UTF_8));
+            writer.println("Server: Welcome to the game!\nServer: Choose one of the following options and try to win.\nServer: But...choose wisely, I know what you are thinking about.\n");
             writer.flush();
             String lineToSend;
             String lineToGet;
 
-
-            while(true){
+            while (true) {
                 lineToGet = reader.readLine();
-                System.out.println(lineToGet);
+                System.out.println("Server: Player chose " + lineToGet);
+
+                if (lineToGet.equals("QUIT")) {
+                    System.out.println("Server: Player left the game\n");
+                    closeClient();
+                    break;
+                }
+
                 Results result = checkWhoWins(lineToGet);
-                TimeUnit.SECONDS.sleep(3);
+                TimeUnit.MILLISECONDS.sleep(0);
                 lineToSend = "Server: I have " + serversChoice + " so, You " + result.name();
                 writer.println(lineToSend);
                 writer.flush();
             }
 
-        } catch(IOException e){
+
+        } catch (IOException e) {
             System.out.println("Blad We/Wy");
             e.printStackTrace();
-        } catch (InterruptedException e){
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
     }
 
-    private void fillWithOptions(){
+    private void closeClient() {
+
+        try {
+            socket.shutdownInput();
+            socket.shutdownOutput();
+            System.out.println("in closeClient()");
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void fillWithOptions() {
         optionsToDraw.add("PAPER");
         optionsToDraw.add("SCISSORS");
         optionsToDraw.add("STONE");
@@ -76,16 +95,16 @@ public class GameHandler implements Runnable {
 
     }
 
-    private Results checkWhoWins(String playersChoice){
+    private Results checkWhoWins(String playersChoice) {
 
         String serversChoice = optionsToDraw.get(random.nextInt(3));
         this.serversChoice = serversChoice;
 
-        if (serversChoice.equals(playersChoice)){
+        if (serversChoice.equals(playersChoice)) {
             return Results.DREW;
         }
 
-        if (firstDefeatsSecond.get(serversChoice).equals(playersChoice)){
+        if (firstDefeatsSecond.get(serversChoice).equals(playersChoice)) {
             return Results.LOST;
         }
 
